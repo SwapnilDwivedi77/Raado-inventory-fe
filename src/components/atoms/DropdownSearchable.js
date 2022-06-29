d709f182b938b3d1cc6c5e9d6bbc663b87afac39
SwapnilDwivedi77 <62413591+SwapnilDwivedi77@users.noreply.github.com>
2022-06-29 23:46:21 +0530

    Added searchable component

--- src/components/atoms/DropdownSearchable.js
+++ src/components/atoms/DropdownSearchable.js
@@ -1,120 +1,45 @@
-import React, { useState, useEffect, useCallback } from 'react';
-
-import {StyleSheet, FlatList, View,TouchableOpacity } from 'react-native';
-import { Colors, StyledText } from '../style';
-import TextInput from './TextInput';
-import debounce from 'lodash.debounce';
+import React from 'react';
+import SearchableDropdown from 'react-native-searchable-dropdown';
+import { Colors,} from '../style';
 import {isEmpty} from '../../utils/index'
 
-const DropdownSearchable = ({getItemSelection,usersList }) => {
-  let timer;
-  const [searchKey, setSearchKey] = useState('')
-  const [filteredUser, setFilteredUSer] = useState(usersList)
-  const [showList,setShowList] = useState('false')
-
-  const filterUser = (value) => {
-
-    if(value.length <3){
-       setFilteredUSer(usersList)
-      return}
-    
-    if (value.length >= 3) {
-      let list = usersList.filter(({ name, phoneNo }) => name.toLowerCase().includes(value.toLowerCase()) || phoneNo.includes(value))
-      setFilteredUSer(list);
-    }
-    else {
-      setFilteredUSer([])
-    }
-  }
-  const debouncedSave = useCallback(
-    debounce(nextValue => filterUser(nextValue), 500),
-    [], // will be created only once initially
-  );
-
-
-  const handleItemSelection = (item) => {
-    getItemSelection(item)
-    setSearchKey(item.name + ', ' + item.phoneNo)
-    setShowList(!showList)
-  }
-
-  const handleChange = value => {
-    setSearchKey(value);
-    debouncedSave(value);
-  };
+const DropdownSearchable = ({getItemSelection,items,selectedItem }) => {
+ 
   return (
-    <View style={{ ...styles.container}}>
-      <TextInput
-        style={{ width: 290 }}
-        icon="supervised-user-circle"
-        placeholder="Search User (Min 3 chars)"
-        placeholderTextColor={Colors.darkLight}
-        onChangeText={handleChange}
-        value={searchKey}
-        onFocus={()=> setShowList(!showList)}
-        
-      />
+    <SearchableDropdown
+    onTextChange={() => {}}
+    onItemSelect={(item) => getItemSelection(item)}
+    containerStyle={{ padding: 5 }}
+    textInputStyle={{
+      padding:15,
+      paddingRight: 55,
+      marginTop: 2,
+      backgroundColor: Colors.secondary,
+      color : Colors.tertiary,
+      borderRadius : 16,
+    }}
+    itemStyle={{
+      padding: 10,
+      marginTop: 2,
+      backgroundColor: '#FAF9F8',
+      borderColor: Colors.primary,
+      borderBottomWidth: 1,
+      paddingLeft : 15
+    }}
+    itemTextStyle={{
+      color: '#222',
+    }}
+    itemsContainerStyle={{
+      maxHeight: '100%',
+      maxWidth : '100%'
+    }}
+    items={items}
+    placeholder={!isEmpty(selectedItem) ? selectedItem.name : 'Select user'}
+    resetValue={false}
+    underlineColorAndroid="transparent"
+  />
 
-     
-
-      {!showList && !isEmpty(filteredUser) && <View style={styles.userList}>
-   
-        <FlatList
-          data={filteredUser}
-          renderItem={({ item }) => (
-            <View style={{ flex: 1, flexDirection: 'column', margin: 1, flexDirection: 'column',  }} key={item.userId}>
-              <TouchableOpacity style={styles.user} key={item.userId}  onPress={()=>handleItemSelection(item)}>
-                <StyledText>{item.name + ',' + item.phoneNo}</StyledText>
-              </TouchableOpacity>
-            </View>
-          )}
-          //Setting the number of column
-          numColumns={1}
-          keyExtractor={(item, index) => item.key}
-        />
-      </View>
-      }
-
-    </View>
   );
 };
 
 export default DropdownSearchable;
-
-const styles = StyleSheet.create({
-  container: {
-    alignSelf: 'center',
-    zIndex: 2000,
-  },
-  titleText: {
-    padding: 8,
-    fontSize: 16,
-    textAlign: 'center',
-    fontWeight: 'bold',
-  },
-  headingText: {
-    padding: 8,
-  },
-
-  userList: {
-    backgroundColor: Colors.primary,
-
-    flex: 1,
-    width: 285,
-    position: 'absolute',
-    zIndex:9999,
-    marginTop : '20%',
-   
-  },
-  user: {
-    paddingTop: 10,
-    paddingBottom: 10,
-    paddingLeft: 10,
-    borderBottomColor: Colors.light,
-    borderBottomWidth: 2,
-    width: "100%",
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor : Colors.light
-  }
-});
