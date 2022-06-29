e8be9d7068222d02cc3f5317d28f7caf260e2e71
SwapnilDwivedi77 <62413591+SwapnilDwivedi77@users.noreply.github.com>
2022-06-29 15:03:11 +0530

    added no access navigator

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,26 +1,27 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { useSelector } from 'react-redux'
 import AuthNavigator from './AuthNavigator';
-import HomeNavigator from './HomeTabNavigator';
 import navigationTheme from './navigationTheme';
-import ScreenHeader from '../components/ScreenHeader';
-import MenuOptionNavigator from './PostLoginNavigator';
 import PostLoginNavigator from './PostLoginNavigator';
+import { isEmpty } from '../utils';
+import NoAccessNavigator from './NoAccessNavigator';
 
 
 
 const AppNavigator = ({ }) => {
 
-    const isLoggedIn = useSelector(state => state.user).isLoggedin;
+    const { isLoggedin, userData } = useSelector(state => state.user);
     return (
-   
-    <NavigationContainer theme={navigationTheme}>
-       {!isLoggedIn ? 
-       <AuthNavigator/> : 
-       <PostLoginNavigator/>
-       }
-    </NavigationContainer>
+
+        <NavigationContainer theme={navigationTheme}>
+            {!isLoggedin ?
+                <AuthNavigator /> :
+                isEmpty(userData.permissions) ?
+                    <NoAccessNavigator /> :
+                    <PostLoginNavigator />
+            }
+        </NavigationContainer>
     )
 
 
